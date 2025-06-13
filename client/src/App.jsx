1fef36a2f6835707c3b454bb4b06978323676911
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2025-06-13 22:29:21 +0530

    Deployed

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>
+        Paragraph 1: This is a simple React application using Vite as the build tool.
+      </p>
+    </>
+  )
+}
+
+export default App
